973f36b3f00747f6ce6564094248c624e5bf0059
rafif76 <64364986+rafif76@users.noreply.github.com>
2020-09-19 21:37:00 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,8 @@
+public class tes{
+	public static void main(String[] args){
+		System.out.print("Nama : rafif khoiron\n");
+		System.out.print("NIM : 6706194080\n");
+		System.out.print("Kelas : 43-03\n");
+
+	}
+}
